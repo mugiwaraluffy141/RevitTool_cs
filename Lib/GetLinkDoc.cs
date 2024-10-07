ae69721c83822a2239b9c164d9ed9695b93b648d
Minh Tran <108816485+mugiwaraluffy141@users.noreply.github.com>
2024-10-07 08:47:37 +0700

    Add files via upload

---
+++ Lib/GetLinkDoc.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhTranTools
+{
+
+    public class GetLinkDoc
+    {
+        public static List<Document> GetLinkedDoc(Document doc)
+        {
+            List<RevitLinkInstance> linkInstances = new FilteredElementCollector(doc).OfClass(typeof(RevitLinkInstance)).Cast<RevitLinkInstance>().ToList();
+            List<Document> linkDoc = new List<Document>();
+            if (linkInstances.Count > 0)
+            {
+                foreach (RevitLinkInstance link in linkInstances)
+                {
+                    linkDoc.Add(link.GetLinkDocument());
+                }
+            }
+            else { return null; }
+            return linkDoc;
+        }
+
+        public static List<string> GetLinkedDocName(Document doc)
+        {
+            List<RevitLinkInstance> linkInstances = new FilteredElementCollector(doc).OfClass(typeof(RevitLinkInstance)).Cast<RevitLinkInstance>().ToList();
+            List<string> linkName = new List<string>();
+
+            if (linkInstances.Count > 0)
+            {
+                foreach (RevitLinkInstance link in linkInstances)
+                {
+                    linkName.Add(link.Name);
+                }
+            } else { return null; }
+
+            return linkName;
+        }
+    }
+}
---
+++ Lib/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhTranTools
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+        public Item(string name, bool isChecked)
+        {
+            Name = name;
+            IsChecked = isChecked;
+        }
+    }
+}
